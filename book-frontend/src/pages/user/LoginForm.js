5070c66698563a80485ec6b9c37ee75b5ceec351
kimeuntae <rladmsxo6533@gmail.com>
2023-03-18 17:34:58 +0900

    AAAA

--- book-frontend/src/pages/user/LoginForm.js
+++ book-frontend/src/pages/user/LoginForm.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 
 const LoginForm = () => {
-  return <div>로그인</div>;
+  const login = () => {
+    sessionStorage.setItem(
+      'user',
+      JSON.stringify({
+        id: 'test',
+        email: 'test1',
+        text: 'dfdf',
+      }),
+    );
+  };
+
+  const clearStorage = () => {
+    sessionStorage.clear();
+    window.location.reload();
+  };
+
+  return (
+    <div>
+      <Button onClick={login}>로그인</Button>
+      <Button onClick={clearStorage}>로그아웃</Button>
+    </div>
+  );
 };
 
 export default LoginForm;
